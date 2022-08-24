f13cc1eb67660e266d5a568e5735e0712a176114
orangelie <sw031489@naver.com>
2022-08-24 22:51:58 +0900

    Matrix4 Working...
    Matrix4 Working...

--- Physics/Matrix4.h
+++ Physics/Matrix4.h
@@ -16,5 +16,27 @@ public:
 
 		real data[12];
 	};
+
+	Matrix4()
+	{
+		data[1] = data[2] = data[3] = data[4] = data[6] = data[7] = data[8] = data[9] = data[11] = 0.0f;
+		data[0] = data[5] = data[10] = 1.0f;
+	}
+
+	void setDiagonal(const real a, const real b, const real c)
+	{
+		data[0] = a;
+		data[5] = b;
+		data[10] = c;
+	}
+
+	Matrix4 operator*(const Matrix4& muliplier) const
+	{
+		Matrix4 m;
+
+
+
+		return m;
+	}
 };
 
